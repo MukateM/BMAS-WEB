9332ecdd3c2c64eddec7420948b29540932a1329
MukateM <mukatemukate93@gmail.com>
2026-05-10 09:37:23 +0300

    hh

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
+import { resolve } from 'path'
+
 export default {
   build: {
     sourcemap: false,
     minify: 'esbuild',
     rollupOptions: {
       input: {
-        main: 'index.html',
-        quiz: 'employment-law-quiz.html'
+        main: resolve(__dirname, 'index.html'),
+        quiz: resolve(__dirname, 'employment-law-quiz.html'),
+        about: resolve(__dirname, 'about.html'),
+        careers: resolve(__dirname, 'careers.html'),
+        clients: resolve(__dirname, 'clients.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        market: resolve(__dirname, 'market.html'),
+        services: resolve(__dirname, 'services.html'),
+        'why-us': resolve(__dirname, 'why-us.html'),
+        'payroll-calculator': resolve(__dirname, 'payroll-calculator.html'),
       }
     }
   }
